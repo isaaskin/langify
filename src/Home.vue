a1860abf13611330d48fa4c097248c27ee3dad89
Isa Askin <isa33649@gmail.com>
2023-02-23 01:02:37 +0100

    Added router and admin page

---
+++ src/Home.vue
@@ -0,0 +1,199 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, query, orderBy, limit, documentId, where } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+
+
+interface Word {
+  en: string,
+  tr: string,
+  id: number
+}
+
+interface Question {
+  context: Word,
+  choices: Word[]
+}
+
+const name = ref<string>('isa samed');
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARxPHJYjVLOJjq6lzxh9WZnh1vyYgtj04",
+  authDomain: "langify-45b13.firebaseapp.com",
+  projectId: "langify-45b13",
+  storageBucket: "langify-45b13.appspot.com",
+  messagingSenderId: "796821918654",
+  appId: "1:796821918654:web:32e461599e0b15050edfef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in.
+    alert("Yes user " + user.email)
+    // ...
+  } else {
+    // User is not signed in.
+    // ...
+    alert("No user")
+  }
+});
+
+var lastId : number;
+
+const q = query(collection(db, "words"), orderBy("id", "desc"), limit(1))
+
+var words = [] as Word[];
+
+const question = ref<Question>({} as Question);
+
+getDocs(q).then((doc) => {
+  lastId = doc.docs[0].data().id;
+  nextQuestion();
+});
+
+const nextQuestion = () => {
+  words = [];
+  let docs = document.getElementsByClassName('choice');
+  for (let i = 0; i < docs.length; i++) {
+    docs[i].classList.remove('wrong')
+    docs[i].classList.remove('correct')
+  }
+  generateRandomNumbers(lastId, 4).forEach(async n => {
+    let word = await getWordById(n);
+    words.push({
+      en: word.data().en,
+      tr: word.data().tr,
+      id: word.data().id
+    });
+    question.value = generateQuestion(words);
+  });
+}
+
+const generateRandomNumbers = (max: number, count: number): number[] => {
+  const randomNumbers = [] as number[];
+
+  while (randomNumbers.length < count) {
+    let random = Math.floor(Math.random() * max);
+    if (randomNumbers.includes(random)) {
+      continue;
+    }
+    randomNumbers.push(random);
+  }
+
+  return randomNumbers;
+}
+
+const getWordById = async (id: number) => {
+  const doc = await getDocs(
+    query(collection(db, "words"), where("id", "==", id))
+  );
+  return doc.docs[0];
+}
+
+const generateQuestion = (words: Word[]) => {
+  let randomNumbers = generateRandomNumbers(words.length, 1);
+  return {
+    context: words[randomNumbers[0]],
+    choices: words
+  } as Question;
+}
+
+const onChoiceMake = (event: MouseEvent) => {
+  let target = event.target! as HTMLElement; 
+  if (question.value.context.tr === target.innerHTML) {
+    target.classList.add('correct')
+    setTimeout(() => {
+      nextQuestion();
+    }, 350)
+  } else {
+    target.classList.add('wrong')
+  }
+}
+
+const email = ref<string>('');
+const password = ref<string>('');
+
+
+const login = () => {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert(user.email)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+const logout = () => {
+  signOut(auth).then()
+}
+
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="email">
+
+  </div>
+  <div>
+    <input type="text" v-model="password">
+  </div>
+  <button @click="login">Login</button>
+  <button @click="logout">Logout</button>
+  <div id="qbox" v-if="Object.keys(question).length > 0">
+    <div id="word">
+      {{ question.context.en }}
+    </div>
+    <div id="choices">
+      <div class="choice" @click="onChoiceMake"  v-for="c in question.choices">{{ c.tr }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #qbox {
+    width: 250px;
+  }
+  #word {
+    padding: 15px;
+    text-align: center;
+  }
+  #choices {
+    display: flex;
+    flex-direction: column;
+  }
+  .choice {
+    background-color: lightblue;
+    color: #121212;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+    border-radius: 10px;
+    transition: .35s ease;
+  }
+  .wrong {
+    background-color: red;
+    color: white;
+  }
+  .correct {
+    background-color: greenyellow;
+  }
+  .choice:hover {
+    cursor: pointer;
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Admin from './Admin.vue'
+import Home from './Home.vue'
 
+import { createRouter, createWebHistory } from 'vue-router'
+
+ 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const routes = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: Admin
+        }
+    ]
+})
+
+const app = createApp(App);
+
+app.use(routes)
+
+app.mount('#app')
